522856936c81d4126a2157bbbe459dc1ebfafe45
naonaorange <nao.lunalovegood@gmail.com>
2018-01-30 19:52:21 +0900

    add command prompt code

---
+++ cs_CommandPrompt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_CommandPrompt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = new cs_CommandPrompt("dir");
+            cmd.Execute();
+
+            System.Console.WriteLine(cmd.ExitCode);
+            System.Console.WriteLine(cmd.StdOut);
+        }
+    }
+}
---
+++ cs_CommandPrompt/cs_CommandPrompt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_CommandPrompt
+{
+    class cs_CommandPrompt
+    {
+        private System.Diagnostics.Process process;
+
+        public string StdOut { get; private set; }
+        public int ExitCode { get; private set; }
+
+        public bool IsWaitForExit { get; set; }
+
+        public cs_CommandPrompt(string arg)
+        {
+            process = new Process();
+
+            process.StartInfo.FileName = "cmd";
+            process.StartInfo.Arguments = "/C " + arg;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+        }
+
+        public void Execute()
+        {
+            process.Start();
+            if (IsWaitForExit)
+            {
+                process.WaitForExit();
+                ExitCode = process.ExitCode;
+            }
+            StdOut = process.StandardOutput.ReadToEnd();
+        }
+    }
+}
