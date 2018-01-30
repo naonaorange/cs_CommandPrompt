504fa3abcd9a6269005d65b177fdad28ef1b79e8
naonaorange <nao.lunalovegood@gmail.com>
2018-01-31 06:49:45 +0900

    add example2,3

--- cs_CommandPrompt/Program.cs
+++ cs_CommandPrompt/Program.cs
@@ -10,8 +10,26 @@ namespace cs_CommandPrompt
     {
         static void Main(string[] args)
         {
+            
+            //Example1
+
             var cmd = new cs_CommandPrompt("dir");
             cmd.Execute();
+            
+
+            //Example2
+            /*
+            var cmd = new cs_CommandPrompt();
+            cmd.SetCommand("echo helloworld");
+            cmd.Execute();
+            */
+
+            //Example3
+            /*
+            var cmd = new cs_CommandPrompt();
+            cmd.Execute("echo hoge");
+            */
+
 
             System.Console.WriteLine(cmd.ExitCode);
             System.Console.WriteLine(cmd.StdOut);
